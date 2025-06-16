473acf686d8fa56936f0858343d93621c0809473
Amandeep Saxena <amandeep.saxena@sourcefuse.com>
2025-06-17 01:01:57 +0530

    Create menu-driven main.py and load data from CSV at startup

---
+++ task-6/main.py
@@ -0,0 +1,39 @@
+from income import add_income
+from expense import add_expense
+from transaction import view_transactions, delete_transaction
+from balance import view_balance
+import transactions as data
+from csv_handler import load_transactions_from_csv
+
+def main():
+    transactions = load_transactions_from_csv()
+
+    while True:
+        print("\n==== Finance Tracker ====")
+        print("1. Add Income")
+        print("2. Add Expense")
+        print("3. View Transactions")
+        print("4. Delete Transaction")
+        print("5. View Balance")
+        print("6. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            add_income()
+        elif choice == '2':
+            add_expense()
+        elif choice == '3':
+            view_transactions()
+        elif choice == '4':
+            delete_transaction()
+        elif choice == '5':
+            view_balance()
+        elif choice == '6':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Try again.")
+
+if __name__ == "__main__":
+    main()
