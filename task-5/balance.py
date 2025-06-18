dd624015153cd1d91b518f95328de5df50a1d8d1
Amandeep Saxena <amandeep.saxena@sourcefuse.com>
2025-06-18 20:15:56 +0530

    correct code for print

--- task-5/balance.py
+++ task-5/balance.py
@@ -1,7 +1,7 @@
-from transactions import transactions
-
-def view_balance():
-    income = sum(txn["amount"] for txn in transactions if txn["type"] == "income")
-    expense = sum(txn["amount"] for txn in transactions if txn["type"] == "expense")
-    balance = income - expense
-    print(f"\n Balance Summary:\nIncome: ₹{income} | Expense: ₹{expense} | Balance: ₹{balance}")
+def view_balance(transactions):
+    income_total = sum(txn[1] for txn in transactions if txn[0] == "income")
+    expense_total = sum(txn[1] for txn in transactions if txn[0] == "expense")
+    balance = income_total - expense_total
+    print(f"Total Income: ₹{income_total}")
+    print(f"Total Expense: ₹{expense_total}")
+    print(f"Balance: ₹{balance}")
