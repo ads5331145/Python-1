d25f74e0a0a0ccca8d857d96a0adb10900809c15
Amandeep Saxena <amandeep.saxena@sourcefuse.com>
2025-06-18 20:16:32 +0530

    correct code for print

--- task-5/main.py
+++ task-5/main.py
@@ -4,8 +4,10 @@ from transaction import view_transactions, delete_transaction
 from balance import view_balance
 
 def main():
+    transactions = []
+
     while True:
-        print("\n Finance Tracker Menu")
+        print("\nFinance Tracker")
         print("1. Add Income")
         print("2. Add Expense")
         print("3. View Transactions")
@@ -15,21 +17,21 @@ def main():
 
         choice = input("Enter your choice (1-6): ")
 
-        if choice == '1':
-            add_income()
-        elif choice == '2':
-            add_expense()
-        elif choice == '3':
-            view_transactions()
-        elif choice == '4':
-            delete_transaction()
-        elif choice == '5':
-            view_balance()
-        elif choice == '6':
-            print("Exiting program.")
+        if choice == "1":
+            add_income(transactions)
+        elif choice == "2":
+            add_expense(transactions)
+        elif choice == "3":
+            view_transactions(transactions)
+        elif choice == "4":
+            delete_transaction(transactions)
+        elif choice == "5":
+            view_balance(transactions)
+        elif choice == "6":
+            print("Exiting Finance Tracker. Goodbye!")
             break
         else:
-            print("Invalid choice. Please try again.")
+            print("Invalid choice. Please enter a number from 1 to 6.")
 
 if __name__ == "__main__":
     main()
